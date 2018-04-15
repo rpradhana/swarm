d040b299140dc1b3e5186667f884ed8e03a1caf1
rpradhana <raymond.pradhana@gmail.com>
2018-04-16 06:34:20 +0800

    add gulp clean runner

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var clean = require('gulp-clean')
 var sourcemaps = require('gulp-sourcemaps');
 var rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
 var bulkSass = require('gulp-sass-bulk-import');
-
 
 gulp.task('sass', function () {
   gulp.src('./assets/css/custom.scss')
@@ -23,4 +23,10 @@ gulp.task('sass', function () {
 
 gulp.task('sass:watch', function () {
   gulp.watch('./assets/css/**/*.scss', ['sass']);
+});
+
+gulp.task('clean', function() {
+  return gulp.src('static/uploads/')
+    .pipe(clean({force: true}))
+    .pipe(gulp.dest('dist'));
 });
